b24fe8bf4821ec83a9787e9c3f91bc7bda10bfa2
izzulputra <izzulislam97@gmail.com>
2022-02-14 11:38:11 +0700

    adding models to define the schema of our database

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const campground = require('./models/campground');
 
+
+mongoose.connect('mongodb://localhost:27017/camping', {
+    useNewUrlParser: true, // version 6++ is no longer necessary
+    useUnifiedTopology: true // its too
+});
+
+//test theese connections
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const app = express(); // to be short
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views')) // we can call the views folder from anywhere
 
-
+// Our Homepage
 app.get('/', (req,res) => {
-    res.render('home')
+    res.render('home') //from views folder
 })
+
+app.get('/campground', async (req, res) => {
+    const camp = new campground({title: 'My Backyard', description: 'cheap camping'}) // call campground func to declarate a new model with new object
+    await camp.save(); // saved to database
+    res.send(camp);
+
+})
+//making a server
 app.listen(3000, ()=> {
     console.log('serving on port 3000')
 })
---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // to be shorter way
+
+const CampgroundSchema = new Schema({
+    title: String,
+    perice: String,
+    description: String,
+    location: String
+});
+
+//making collection in dbs, which exported to app.js and linked to const camp in app js
+module.exports = mongoose.model('Campground', CampgroundSchema);
